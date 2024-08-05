b41ff7afb39a314333d57c4f0a28875cc9292437
Nayini <nayiniswapnika145@gmail.com>
2024-08-05 16:31:09 +0100

    pm change

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const hourE1=document.getElementById("hour");
-const minutesE1=document.getElementById("minutes");
+const minuteE1=document.getElementById("minutes");
 const secondE1=document.getElementById("seconds");
 const ampmE1= document.getElementById("ampm");
 
@@ -11,19 +11,20 @@ function updateClock(){
 
     if(h>12){
         h=h-12;
-        ampm="PM";
+        ampm ="PM";
     }
+
     h=h<10?"0"+h:h;
     m=m<10?"0"+m:m;
     s=s<10?"0"+s:s;
 
 
     hourE1.innerText =h;
-    minutesE1.innerText=m;
+    minuteE1.innerText=m;
     secondE1.innerText=s;
-    ampmE1,(innerText=ampm);
+    ampmE1.innerText = ampm;
     setTimeout( ()=>{
-    updateClock()
-    }, 1000)
+    updateClock();
+    }, 1000);
 }
 updateClock();
